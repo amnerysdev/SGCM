43a03358abd416c2cbfe9f53d228ac0d1b1a21fd
BrailyRS <78430276+Dextron03@users.noreply.github.com>
2026-06-12 11:13:39 -0400

    He agregado el DbContext el cual hereda de identity

---
+++ SGCM.Data/Context/SgcmDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using SGCM.Data.Entities;
+
+namespace SGCM.Data.Context
+{
+    public class SgcmDbContext : IdentityDbContext<AppUser>
+    {
+        public SgcmDbContext(DbContextOptions<SgcmDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<MedicalRecord> MedicalRecords { get; set; }
+        public DbSet<Specialty> Specialties { get; set; }
+
+    }
+}
