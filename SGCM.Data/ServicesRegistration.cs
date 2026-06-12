cf2b0d9b53301f132e3545d8f03aa7b3d5d2e5a4
BrailyRS <78430276+Dextron03@users.noreply.github.com>
2026-06-12 16:50:04 -0400

    He agregado el ServiceRegistration de Data

--- SGCM.Data/ServicesRegistration.cs
+++ SGCM.Data/ServicesRegistration.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SGCM.Data.Context;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +13,28 @@ namespace SGCM.Data
 {
     public static class ServicesRegistration
     {
+        public static void AddDataLayerIoc(this IServiceCollection service, IConfiguration configuration)
+        {
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                service.AddDbContext<SgcmDbContext>(options
+                    => options.UseInMemoryDatabase("SGCM"));
+            }
+            else 
+            {
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                service.AddDbContext<SgcmDbContext>(
+                    (serviceProvider, opt) =>
+                    {
+                        opt.EnableSensitiveDataLogging();
+                        opt.UseSqlServer(connectionString,
+                        m => m.MigrationsAssembly(typeof(SgcmDbContext).Assembly.FullName));
+                    },
+                    contextLifetime: ServiceLifetime.Scoped,
+                    optionsLifetime: ServiceLifetime.Scoped
+                );
+            }
+        } 
 
     }
 }
