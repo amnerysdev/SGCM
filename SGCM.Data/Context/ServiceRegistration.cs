92ea17a57e418ae22a3c023d5baaccc9837f2718
BrailyRS <78430276+Dextron03@users.noreply.github.com>
2026-06-12 12:05:05 -0400

    He hecho la configuracion de Identity para el proyecto.

---
+++ SGCM.Data/Context/ServiceRegistration.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using SGCM.Data.Entities;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGCM.Data.Context
+{
+    public static class ServiceRegistration 
+    {
+        public static void AddIdentityService(this IServiceCollection services, IConfiguration configuration)
+        {
+            // 1. Configuración del motor de Identity
+            services.AddIdentity<AppUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 8;
+                options.User.RequireUniqueEmail = true;
+            })
+                .AddEntityFrameworkStores<SgcmDbContext>()
+                .AddDefaultTokenProviders();
+
+            // 2. JWT Configuration
+            services.Configure<JWTSettings>(configuration.GetSection("JWTSettings"));
+
+            services.AddAuthentication(options =>
+            {
+
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                    ValidIssuer = configuration["JWTSettings:Issuer"],
+                    ValidAudience = configuration["JWTSettings:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings"] ?? string.Empty))
+                };
+            });
+        }
+    }
+}
