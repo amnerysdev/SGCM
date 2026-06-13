ccda55a8a33a65e43aab5ee8573c2324bf07b12f
@amnerysdev <256653424+amnerysdev@users.noreply.github.com>
2026-06-13 11:28:26 -0400

    Agregar entidades Medico y Paciente.

---
+++ SGCM.Data/Entities/Medico.cs
@@ -0,0 +1,15 @@
+﻿namespace SGCM.Data.Entities
+{
+    public class Medico
+    {
+        public int MedicoId { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string CorreoElectronico { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public DateTime FechaDeNacimiento { get; set; }
+        public string Contrasena { get; set; } = string.Empty;
+        public string Especialidad { get; set; } = string.Empty;
+        public string NumeroDeExequatur { get; set; } = string.Empty;
+    }
+}
---
+++ SGCM.Data/Entities/Paciente.cs
@@ -0,0 +1,13 @@
+﻿namespace SGCM.Data.Entities
+{
+    public class Paciente
+    {
+        public int PacienteId { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string CorreoElectronico { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public DateTime FechaDeNacimiento { get; set; }
+        public string Contrasena { get; set; } = string.Empty;
+    }
+}
