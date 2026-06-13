d99cd14b7d8da6e24b8060da1dadeced1076a550
BrailyRS <78430276+Dextron03@users.noreply.github.com>
2026-06-13 14:03:46 -0400

    He agregado los Dtos base para comenzar el proyecto

---
+++ SGCM.Data/DTOs/DoctorDto.cs
@@ -0,0 +1,18 @@
+﻿using SGCM.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGCM.Data.DTOs
+{
+    public class DoctorDto
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string MedicalLicense { get; set; } = string.Empty;
+        public string AppUserId { get; set; } = string.Empty;
+        public string SpecialtyId { get; set; } = string.Empty;
+
+    }
+}
---
+++ SGCM.Data/DTOs/PatientDto.cs
@@ -0,0 +1,20 @@
+﻿using SGCM.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGCM.Data.DTOs
+{
+    public class PatientDto
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string SocialSecurityNumber { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; } = string.Empty;
+
+        public string AppUserId { get; set; } = string.Empty;
+
+    }
+}
