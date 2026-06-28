8c6cba977cd17f849423e4de76f12c1e396f9655
@amnerysdev <256653424+amnerysdev@users.noreply.github.com>
2026-06-28 17:18:01 -0400

    Agregar interfaz y repositorio generico para operaciones basicas de acceso de datos (CRUD).

---
+++ SGCM.Data/Interfaces/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace SGCM.Data.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(string id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ SGCM.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGCM.Data.Context;
+using SGCM.Data.Interfaces;
+
+namespace SGCM.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly SgcmDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(SgcmDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(string id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
